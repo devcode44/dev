6664e56d098467edce07f146c24bec295d545b90
devcode44 <114732624+devcode44@users.noreply.github.com>
2022-11-03 12:22:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,22 @@
 $(document).ready(function(){
-    $("#python").click(function(){
-        $(".py , #text-content-center-py").css({"display": "block"})
-        $(".java , #text-content-center-java").css({"display": "none"})
-        $(".php , #text-content-center-php").css({"display": "none"})
+  $("#pr").click(function(){
+      $(".pr , #text-content-center-py").css({"display": "block"})
+      $(".em , #text-content-center-java").css({"display": "none"})
+      $("#insta , #text-content-center-js").css({"display": "none"})
 
-    })
+  })
 })
 $(document).ready(function(){
-    $("#java").click(function(){
-        $(".java , #text-content-center-java").css({"display": "block"})
-        $(".py , #text-content-center-py").css({"display": "none"})
-        $(".php , #text-content-center-php").css({"display": "none"})
-
-    })
+  $("#em").click(function(){
+      $(".em , #text-content-center-java").css({"display": "block"})
+      $(".pr , #text-content-center-py").css({"display": "none"})
+      $("#insta , #text-content-center-js").css({"display": "none"})
+  })
 })
 $(document).ready(function(){
-    $("#php").click(function(){
-        $(".php , #text-content-center-php").css({"display": "block"})
-        $(".java , #text-content-center-java").css({"display": "none"})
-        $(".py , #text-content-center-py").css({"display": "none"})
-    })
+  $("#sm").click(function(){
+      $("#insta , #text-content-center-js").css({"display": "block"})
+      $(".em , #text-content-center-java").css({"display": "none"})
+      $(".pr , #text-content-center-py").css({"display": "none"})
+  })
 })
-$(document).ready(function(){
-    $("#html").click(function(){
-        $(".html , #text-content-center-html").css({"display": "block"})
-        $(".css , #text-content-center-css").css({"display": "none"})
-        $(".js , #text-content-center-js").css({"display": "none"})
-    })
-})
-$(document).ready(function(){
-    $("#css").click(function(){
-        $(".css , #text-content-center-css").css({"display": "block"})
-        $(".html , #text-content-center-html").css({"display": "none"})
-        $(".js , #text-content-center-js").css({"display": "none"})
-    })
-})
-$(document).ready(function(){
-    $("#javascript").click(function(){
-        $(".js , #text-content-center-js").css({"display": "block"})
-        $(".css , #text-content-center-css").css({"display": "none"})
-        $(".html , #text-content-center-html").css({"display": "none"})
-    })
-})
-
-function replacepage() {
-    var newElement = 
-
-
-    document.body.innerHTML = newElement;
-}
-
-
-
-
